7e6f155f7fbe7273ad248665063e1f1a2f87bf19
Roberto Carlo Camacho Fernandez <camachof.roberto@gmail.com>
2018-11-26 17:19:19 -0200

    continuando com o projeto

--- course/gulpfile.js
+++ course/gulpfile.js
@@ -6,17 +6,20 @@ const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
 const htmlmin = require('gulp-htmlmin');
 const gls = require('gulp-live-server');
+const browserSync = require('browser-sync').create();
 
 
 gulp.task('sass', function () {
     return gulp.src('assets/src/sass/**/*.scss')
         .pipe(concat('style.min.css'))
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('assets/css'));
+        .pipe(gulp.dest('assets/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('js', function () {
     return gulp.src('assets/src/js/**/*.js')
+        .pipe(browserify())
         .pipe(concat('script.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('assets/js'));
@@ -38,29 +41,33 @@ gulp.task('htmlmin', function () {
         .pipe(gulp.dest('.'))
 });
 
-gulp.task('watch', function () {
-    gulp.watch('assets/src/sass/**/*.scss', gulp.series('sass'));
+gulp.task('js-watch', ['js'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('default', ['js'], function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
     gulp.watch('assets/src/js/**/*.js', gulp.series('js'));
-    gulp.watch('assets/src/img/*', gulp.series('image'));
-    gulp.watch('_html/**/*.html', gulp.series('htmlmin'));
 });
 
-gulp.task('serve', function () {
-    const server = gls.static('./', 8000);
-    server.start();
-    gulp.watch('assets/css/**/*.css', function (file) {
-        gls.notify.apply(server, [file]);
+gulp.task('serve', ['sass'], function () {
+    browserSync.init({
+        server: './'
     });
-    gulp.watch('assets/js/**/*.js', function (file) {
-        gls.notify.apply(server, [file]);
-    });
-    gulp.watch('assets/img/*', function (file) {
-        gls.notify.apply(server, [file]);
-    });
-    gulp.watch('./*.html', function (file) {
-        gls.notify.apply(server, [file]);
-    });
+    gulp.watch('assets/src/sass/**/*.scss', gulp.series('sass'));
+    gulp.watch('/.html').on('change', browserSync.reload);
 
+
+    gulp.watch('assets/src/img/*', gulp.series('image').on('change', browserSync.reload));
+    gulp.watch('/.html').on('change', browserSync.reload);
+
+    gulp.watch('_html/**/*.html', gulp.series('htmlmin').on('change', browserSync.reload));
+    gulp.watch('/.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.parallel ('sass', 'js', 'htmlmin', 'image', 'watch', 'serve'));
+gulp.task('default', gulp.parallel('sass', 'js', 'htmlmin', 'image', 'watch', 'js-watch', 'serve'));
